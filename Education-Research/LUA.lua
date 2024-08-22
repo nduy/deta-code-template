a090c201ee2cdc3aa1c5079261b0ee141be4243d
Duc-Duy Nguyen <duyndx@gmail.com>
2024-08-22 08:20:46 +0100

    Create LUA.lua
    Add simple code for Lua (programming language)

---
+++ Education-Research/LUA.lua
@@ -0,0 +1,15 @@
+
+--[[ Delta code template]]--
+function  main()
+    print("Hello world")
+    return 0
+    end;
+
+
+  
+  myprint= function(param)
+    print("This is my print function -   ##",param,"##")
+  end;
+   
+    
+  myprint("Hello World!") 
