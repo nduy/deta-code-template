72bad9e9113560cda9117c27cdb0e13095af1346
Duy Nguyen <duyndx@gmail.com>
2026-08-16 18:48:56 +0100

    Upload Dart Template
    Dart Template

---
+++ Web-Mobile-IoT/Mobile-IoT/HelloAndAge.dart
@@ -0,0 +1,16 @@
+// DELTA CODE TEMPLATE
+// DART Program
+// Print out 2 variable with assertion
+//
+
+void main(){
+    String name= "Panda";
+    double marks=78;
+    print("Hello Name: $name, Marks: $marks");
+  ////////////////////////////////////////
+  String age = "19";
+
+  assert(int.parse(age) >= 18 && !(age is String), "Age must be number at least 18.");
+  print("Hello Name: $name, Age: $age");
+
+}
